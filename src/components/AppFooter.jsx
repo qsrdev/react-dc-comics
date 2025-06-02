6f08bd6ef48fdddd1c2098d291620adb6ca30bea
qsrdev <matteo.albertini747@gmail.com>
2025-06-02 17:24:03 +0200

    feat: task almost done no background

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,109 +1,153 @@
 function AppFooter() {
   return (
-    <section className="jumbo">
-      <div className="container">
-        <div className="row flex">
-          <div className="col-50 flex-align">
-            <div className="col-33 ">
-              <h3>dc comics</h3>
-              <ul>
-                <li>
-                  <a href="">Characters</a>
-                </li>
-                <li>
-                  <a href="">Comics</a>
-                </li>
-                <li>
-                  <a href="">Movies</a>
-                </li>
-                <li>
-                  <a href="">TV</a>
-                </li>
-                <li>
-                  <a href="">Games</a>
-                </li>
-                <li>
-                  <a href="">Videos</a>
-                </li>
-                <li>
-                  <a href="">News</a>
-                </li>
-              </ul>
-              <h3>dc comics</h3>
-              <ul>
-                <li>
-                  <a href="">Characters</a>
-                </li>
-                <li>
-                  <a href="">Comics</a>
-                </li>
-                <li>
-                  <a href="">Movies</a>
-                </li>
-              </ul>
+    <section>
+      <div className="jumbo">
+        <div className="container">
+          <div className="row flex">
+            <div className="col-50 flex-align">
+              <div className="col-33 ">
+                <h3>dc comics</h3>
+                <ul>
+                  <li>
+                    <a href="">Characters</a>
+                  </li>
+                  <li>
+                    <a href="">Comics</a>
+                  </li>
+                  <li>
+                    <a href="">Movies</a>
+                  </li>
+                  <li>
+                    <a href="">TV</a>
+                  </li>
+                  <li>
+                    <a href="">Games</a>
+                  </li>
+                  <li>
+                    <a href="">Videos</a>
+                  </li>
+                  <li>
+                    <a href="">News</a>
+                  </li>
+                </ul>
+                <h3 className="my-5">dc comics</h3>
+                <ul>
+                  <li>
+                    <a href="">Characters</a>
+                  </li>
+                  <li>
+                    <a href="">Comics</a>
+                  </li>
+                  <li>
+                    <a href="">Movies</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-33 ">
+                <h3>dc</h3>
+                <ul>
+                  <li>
+                    <a href="">Terms of Use</a>
+                  </li>
+                  <li>
+                    <a href="">Privacy Policy (New)</a>
+                  </li>
+                  <li>
+                    <a href="">Ad Choices</a>
+                  </li>
+                  <li>
+                    <a href="">Advertising</a>
+                  </li>
+                  <li>
+                    <a href="">Jobs</a>
+                  </li>
+                  <li>
+                    <a href="">Subscriptions</a>
+                  </li>
+                  <li>
+                    <a href="">Talent Workshops</a>
+                  </li>
+                  <li>
+                    <a href="">CPSC Certificates</a>
+                  </li>
+                  <li>
+                    <a href="">Ratings</a>
+                  </li>
+                  <li>
+                    <a href="">Shop Help</a>
+                  </li>
+                  <li>
+                    <a href="">Contact Us</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-33 ">
+                <h3>Sites</h3>
+                <ul>
+                  <li>
+                    <a href="">DC</a>
+                  </li>
+                  <li>
+                    <a href="">MAD Magazine</a>
+                  </li>
+                  <li>
+                    <a href="">DC Kids</a>
+                  </li>
+                  <li>
+                    <a href="">DC Universe</a>
+                  </li>
+                  <li>
+                    <a href="">DC Power Visa</a>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="col-33 ">
-              <h3>dc</h3>
-              <ul>
-                <li>
-                  <a href="">Terms of Use</a>
-                </li>
-                <li>
-                  <a href="">Privacy Policy (New)</a>
-                </li>
-                <li>
-                  <a href="">Ad Choices</a>
-                </li>
-                <li>
-                  <a href="">Advertising</a>
-                </li>
-                <li>
-                  <a href="">Jobs</a>
-                </li>
-                <li>
-                  <a href="">Subscriptions</a>
-                </li>
-                <li>
-                  <a href="">Talent Workshops</a>
-                </li>
-                <li>
-                  <a href="">CPSC Certificates</a>
-                </li>
-                <li>
-                  <a href="">Ratings</a>
-                </li>
-                <li>
-                  <a href="">Shop Help</a>
-                </li>
-                <li>
-                  <a href="">Contact Us</a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-33 ">
-              <h3>Sites</h3>
-              <ul>
-                <li>
-                  <a href="">that shit fells good</a>
-                </li>
-                <li>
-                  <a href="">that shit fells good</a>
-                </li>
-                <li>
-                  <a href="">that shit fells good</a>
-                </li>
-                <li>
-                  <a href="">that shit fells good</a>
-                </li>
-                <li>
-                  <a href="">that shit fells good</a>
-                </li>
-              </ul>
+            <div className="col-50 logodc">
+              <img src="../public/img/dc-logo-bg.png" alt="" />
             </div>
           </div>
-          <div className="col-50 logo-bg">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quae deleniti ab consectetur ipsam beatae ea inventore nobis similique vero exercitationem, fugiat labore consequatur neque
-            accusantium vel possimus, eveniet deserunt.
+        </div>
+      </div>
+      <div className="footerBottom">
+        <div className="container ">
+          <div className="row flex inbetween ">
+            <div className="col-50">
+              <a href="" className="signup">
+                Sign-Up Now!
+              </a>
+            </div>
+            <div className="col-50 flex">
+              <h3 className="follow-us">Follow us</h3>
+              <nav className="nav">
+                <ul>
+                  <li>
+                    <a href="">
+                      <img src="./img/footer-facebook.png" alt="" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="">
+                      <img src="./img/footer-twitter.png" alt="" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="">
+                      <img src="./img/footer-youtube.png" alt="" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="">
+                      <img src="./img/footer-pinterest.png" alt="" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="">
+                      <img src="./img/footer-periscope.png" alt="" />
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </div>
         </div>
       </div>
